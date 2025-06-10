e6a90d737c931fad144d368961864f61cac05db4
George Roe <george.roe@stfc.ac.uk>
2025-06-10 13:45:25 +0100

    feat: plane geometry

---
+++ src/geometry/plane_geometry.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "geometry.hpp"
+
+class PlaneGeometry : public Geometry {
+public:
+  PlaneGeometry() : normal_(DecimalVector3(0, 1, 0)), point_(DecimalVector3(0, 0, 0)) {}
+  PlaneGeometry(DecimalVector3 normal, DecimalVector3 point) : normal_(normal.normalised()), point_(point) {}
+
+  DecimalVector3 centroid() const override {
+    return point_;
+  }
+
+  Decimal minimumSignedDistanceFrom(DecimalVector3 point) const override {
+    return (point - point_).dot(normal_);
+  }
+
+private:
+  DecimalVector3 normal_;
+  DecimalVector3 point_;
+};
+
