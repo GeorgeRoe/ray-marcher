d8ddb6990404be3ac029ed9768afe31773fe7809
George Roe <george.roe@stfc.ac.uk>
2025-06-10 10:18:33 +0100

    feat: testing with two lights and a mandelbulb

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "rgb.hpp"
 #include "scene.hpp"
 #include "src/geometry/sphere_geometry.hpp"
+#include "src/geometry/mandelbulb_geometry.hpp"
 #include "src/march_options.hpp"
 #include "src/material.hpp"
 #include "vector3.hpp"
@@ -14,22 +15,27 @@ int main() {
   Scene scene;
 
   scene.addObject(Object(
-    std::make_unique<SphereGeometry>(DecimalVector3(7, 0, -1.5), 1),
-    Material(RGB(255, 100, 100), 0, {0, 0, 0})
+    std::make_unique<MandelbulbGeometry>(DecimalVector3(7, 0, -1.5), 1),
+    Material(RGB(255, 100, 100), 0.25, {0, 0, 0})
   ));
 
   scene.addObject(Object(
     std::make_unique<SphereGeometry>(DecimalVector3(7, 0, 1.5), 1),
-    Material(RGB(100, 100, 255), 0.5, {0, 0, 0})
+    Material(RGB(100, 100, 255), 0, {0, 0, 0})
   ));
 
   scene.addObject(Object(
-    std::make_unique<SphereGeometry>(DecimalVector3(5, 2, 0), 0.1),
+    std::make_unique<SphereGeometry>(DecimalVector3(5, 2, 0), 1),
+    Material({0, 0, 0}, 0.0, RGB(255, 255, 255))
+  ));
+
+  scene.addObject(Object(
+    std::make_unique<SphereGeometry>(DecimalVector3(5, -2, 0), 1),
     Material({0, 0, 0}, 0.0, RGB(255, 255, 255))
   ));
 
   IntegerVector2 resolution = {80, 45};
-  int scale = 25;
+  int scale = 100;
   resolution.x *= scale;
   resolution.y *= scale;
   Decimal aspect_ratio = static_cast<Decimal>(resolution.y) / resolution.x;
@@ -38,7 +44,7 @@ int main() {
   Decimal vertical_fov = horizontal_fov * aspect_ratio;
   DecimalVector2 fov((Decimal(horizontal_fov)), Decimal(vertical_fov));
 
-  Camera camera({}, {1, 0, 0}, resolution, fov);
+  Camera camera({2, 0, 0}, {1, 0, 0}, resolution, fov);
 
   int width = camera.getScreenSize().x;
   int height = camera.getScreenSize().y;
